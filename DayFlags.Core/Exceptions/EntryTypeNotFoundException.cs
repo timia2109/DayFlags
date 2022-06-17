18985bbdb5598b81d34ad0c2fcf18b02ec66ebae
Tim Ittermann <tim.ittermann@gmail.com>
2022-06-17 13:38:51 +0200

    Add EntryTypeNotFoundException

---
+++ DayFlags.Core/Exceptions/EntryTypeNotFoundException.cs
@@ -0,0 +1,19 @@
+using DayFlags.Core.Models;
+
+namespace DayFlags.Core.Exceptions;
+
+/// <summary>
+/// Signals, that a <see cref="EntryType"/> was not found
+/// </summary>
+[System.Serializable]
+public class EntryTypeNotFoundException : ARestException
+{
+    public EntryTypeNotFoundException(string entryTypeId) : base(
+        $"{nameof(EntryType)} not found"
+    )
+    {
+        Detail = $"The {nameof(EntryType)} with the id '{entryTypeId}' was not found";
+    }
+
+    public override int StatusCode => StatusCodes.Status404NotFound;
+}
