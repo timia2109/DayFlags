71f53cd558d4eac948aee4e1324cedb2f301fb6d
Tim Ittermann <tim.ittermann@gmail.com>
2024-01-25 18:36:01 +0100

    Move to ranges

--- DayFlags.Core/Repositories/IDayEntryRepository.cs
+++ DayFlags.Core/Repositories/IDayEntryRepository.cs
@@ -1,4 +1,5 @@
 using DayFlags.Core.Models;
+using DayFlags.Core.Util;
 
 namespace DayFlags.Core.Repositories;
 
@@ -6,23 +7,19 @@ public interface IDayFlagRepository
 {
     Task<DayFlag> GetDayFlagAsync(Guid flagId);
     Task<IEnumerable<DayFlag>> GetDayFlagsAsync(Realm realm,
-        DateOnly rangeStart,
-        DateOnly rangeEnd);
+        DateRange dateRange);
     Task<IEnumerable<DayFlag>> GetDayFlagsAsync(FlagType flagType,
-        DateOnly rangeStart,
-        DateOnly rangeEnd);
+        DateRange dateRange);
     Task<IEnumerable<DayFlag>> GetDayFlagsAsync(
         IEnumerable<FlagType> flagTypes,
-        DateOnly rangeStart,
-        DateOnly rangeEnd);
+        DateRange dateRange);
     Task<DayFlag> AddDayFlagAsync(DayFlag dayFlag);
     Task<DayFlag> DeleteDayFlagAsync(DayFlag dayFlag);
     Task<IEnumerable<DayFlag>> FindDayFlagsForDateAsync(
         Realm realm,
-        DateOnly date);
+        DateRange dateRange);
     Task<DayFlag?> FindLastFlagAsync(FlagType type);
     Task<DayFlag?> FindFirstFlagAsync(FlagType type);
     Task<int?> CountDayFlagsAsync(FlagType type,
-        DateOnly rangeStart,
-        DateOnly rangeEnd);
+        DateRange dateRange);
 }
