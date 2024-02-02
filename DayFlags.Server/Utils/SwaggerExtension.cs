3bdb6db6f367d4904e29642cf0e3a9f998e6cd15
Tim Ittermann <tim.ittermann@gmail.com>
2024-02-02 09:45:51 +0100

    Move Swagger Configuration

---
+++ DayFlags.Server/Utils/SwaggerExtension.cs
@@ -0,0 +1,31 @@
+using Microsoft.OpenApi.Models;
+
+namespace DayFlags.Server.Utils;
+
+public static class SwaggerExtensions
+{
+    public static void AddDayFlagSwagger(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1",
+                new OpenApiInfo
+                {
+                    Title = "DayFlags API - v1",
+                    Version = "v1"
+                }
+            );
+
+            var filePath = Path.Combine(AppContext.BaseDirectory, "DayFlags.Server.xml");
+            c.IncludeXmlComments(filePath);
+        });
+    }
+
+    public static void UseDayFlagSwagger(this WebApplication app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+}
