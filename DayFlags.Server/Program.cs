78c7b6838f25ee1a751ebc96c6ecf9716e300073
Tim Ittermann <tim.ittermann@gmail.com>
2024-02-02 07:42:57 +0100

    Remove old resources

--- DayFlags.Server/Program.cs
+++ DayFlags.Server/Program.cs
@@ -1,32 +1,18 @@
 using DayFlags;
-using DayFlags.Core.EntryTypes;
-using DayFlags.Core.MatchProvider;
-using DayFlags.Server.Converters;
-using DayFlags.Server.EntryTypes;
-using DayFlags.Server.MatchProvider;
 using DayFlags.Server.Middlewares;
-using DayFlags.Server.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddScoped<EntryTypeService>();
-builder.Services.AddScoped<DayEntriesService>();
-builder.Services.AddScoped<MatchingService>();
-builder.Services.AddScoped<IMatchProvider, DatabaseMatchProvider>();
-builder.Services.AddScoped<AEntryTypeProvider, DbEntryTypesProvider>();
+
 
 builder.Services.AddDbContext<DayFlagsDb>(options =>
 {
     options.UseSqlite("Data Source=DayFlags.db;");
 });
 builder.Services
-    .AddControllers()
-    .AddNewtonsoftJson(options =>
-    {
-        options.SerializerSettings.Converters.Add(new DateOnlyConverter());
-    });
+    .AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
